cd47af1d86a074db5de3e60ee5770cdcbc832652
your mom <dababyismydad2@gmail.com>
2021-12-10 07:55:02 -0600

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by AM Lisp - Luis Robles-Ibarra on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
